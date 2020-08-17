47809268648632520ec0971d24c00e8d2dfded4f
Carlos Velasquez <carlosrobertovelasquez@gmail.com>
2020-08-16 23:46:14 -0600

    En este resolver se agregan follows y publish tanto mutation como query

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,14 +1,26 @@
 const userController = require('../controlles/User');
-const User = require('../models/User');
+const followController = require('../controlles/Follow');
+const publicationController = require('../controlles/Publication');
+
 const resolvers = {
 	Query: {
+		//User
 		getUser: (_, { id, email }) => userController.getUser(id, email),
-		search: (_, { search }) => userController.search(search)
+		search: (_, { search }) => userController.search(search),
+		//Follow
+		isFollow: (_, { email }, ctx) => followController.isFollow(email, ctx),
+		getFollowers: (_, { email }) => followController.getFollowers(email)
 	},
 	Mutation: {
+		//User
 		newUser: async (_, { input }) => userController.newUser(input),
 		authenticateUser: async (_, { input }) => userController.authenticateUser(input),
-		updateAvatar: (_, { file }, ctx) => userController.updateAvatar(file, ctx)
+		updateAvatar: (_, { file }, ctx) => userController.updateAvatar(file, ctx),
+		//Follow
+		follow: (_, { email }, ctx) => followController.follow(email, ctx),
+		unFollow: (_, { email }, ctx) => followController.unFollow(email, ctx),
+		//Publication
+		publish: (_, { file }, ctx) => publicationController.publish(file, ctx)
 	}
 };
 
